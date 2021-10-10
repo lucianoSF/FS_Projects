d2a6c45fd3610925114c12572653dbc55d4fc6ff
lucianoSF <lukfraga@hotmail.com>
2021-10-09 22:51:17 -0300

    Project 3

---
+++ fs20211Aula3/src/main/java/br/ufg/inf/fs20211/entities/Hospedagem.java
@@ -0,0 +1,102 @@
+package br.ufg.inf.fs20211.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tb_hospedagem")
+public class Hospedagem implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id_hospedagem")
+    private Integer idHospedagem;
+
+    @OneToOne
+    @JoinColumn(name="id_quarto")
+    private Quarto quarto;
+
+    @OneToOne
+    @JoinColumn(name="id_hospede")
+    private Hospede hospede;
+
+    @Column(name="dt_checkin")
+    private String dtCheckin;
+
+    @Column(name="dt_checkout")
+    private String dtCheckout;
+
+    public Hospedagem() {
+        super();
+    }
+
+    public Hospedagem(Integer idHospedagem, Quarto quarto, Hospede hospede, String dtCheckin, String dtCheckout) {
+        super();
+        this.idHospedagem = idHospedagem;
+        this.quarto = quarto;
+        this.hospede = hospede;
+        this.dtCheckin = dtCheckin;
+        this.dtCheckout = dtCheckout;
+    }
+
+    public Integer getIdHospedagem() {
+        return idHospedagem;
+    }
+
+    public void setIdHospedagem(Integer idHospedagem) {
+        this.idHospedagem = idHospedagem;
+    }
+
+    public Quarto getQuarto() {
+        return quarto;
+    }
+
+    public void setQuarto(Quarto quarto) {
+        this.quarto = quarto;
+    }
+
+    public Hospede getHospede() {
+        return hospede;
+    }
+
+    public void setHospede(Hospede hospede) {
+        this.hospede = hospede;
+    }
+
+    public String getDtCheckin() {
+        return dtCheckin;
+    }
+
+    public void setDtCheckin(String dtCheckin) {
+        this.dtCheckin = dtCheckin;
+    }
+
+    public String getDtCheckout() {
+        return dtCheckout;
+    }
+
+    public void setDtCheckout(String dtCheckout) {
+        this.dtCheckout = dtCheckout;
+    }
+
+    @Override
+    public String toString() {
+        return "Hospedagem{" +
+                "idHospedagem=" + idHospedagem +
+                ", quarto=" + quarto +
+                ", hospede=" + hospede +
+                ", dtCheckin='" + dtCheckin + '\'' +
+                ", dtCheckout='" + dtCheckout + '\'' +
+                '}';
+    }
+}
---
+++ fs20211Aula3/src/main/java/br/ufg/inf/fs20211/exceptions/HospedeException.java
@@ -0,0 +1,10 @@
+package br.ufg.inf.fs20211.exceptions;
+
+public class HospedeException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+    public HospedeException(String msg) {
+        super(msg);
+    }
+}
